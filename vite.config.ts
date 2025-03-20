2d350aa420480aeb549d630bb60b7d73c99cf769
CapablancaYEHA <lulushe4ka@gmail.com>
2025-03-20 13:37:21 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       "@api": path.resolve(__dirname, "./src/api"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
       "@styles": path.resolve(__dirname, "./src/styles"),
     },
   },
