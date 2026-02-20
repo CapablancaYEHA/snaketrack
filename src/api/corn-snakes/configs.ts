b01634d23d411a45402de3c6a862e50aba41d43d
CapablancaYEHA <lulushe4ka@gmail.com>
2026-02-20 21:07:20 +0300

    fix remList config wrong table

--- src/api/corn-snakes/configs.ts
+++ src/api/corn-snakes/configs.ts
@@ -2,6 +2,6 @@ import { disStats } from "@/components/common/Market/utils";
 import { ESupabase } from "../common";
 
 export const csList = (userId) => ({ t: ESupabase.CS, f: (b) => b.eq("owner_id", userId), id: userId });
-export const csRemList = (userId) => ({ t: ESupabase.BP, f: (b) => b.eq("owner_id", userId).filter("status", "not.in", `(${disStats.join(",")})`), id: userId });
+export const csRemList = (userId) => ({ t: ESupabase.CS, f: (b) => b.eq("owner_id", userId).filter("status", "not.in", `(${disStats.join(",")})`), id: userId });
 
 export const csSingle = (id) => ({ t: ESupabase.CS, f: (b) => b.eq("id", id).limit(1).single(), id });
--- src/api/morelia_viridis/configs.ts
+++ src/api/morelia_viridis/configs.ts
@@ -2,6 +2,6 @@ import { disStats } from "@/components/common/Market/utils";
 import { ESupabase } from "../common";
 
 export const mvList = (userId) => ({ t: ESupabase.MV, f: (b) => b.eq("owner_id", userId), id: userId });
-export const mvRemList = (userId) => ({ t: ESupabase.BP, f: (b) => b.eq("owner_id", userId).filter("status", "not.in", `(${disStats.join(",")})`), id: userId });
+export const mvRemList = (userId) => ({ t: ESupabase.MV, f: (b) => b.eq("owner_id", userId).filter("status", "not.in", `(${disStats.join(",")})`), id: userId });
 
 export const mvSingle = (id) => ({ t: ESupabase.MV, f: (b) => b.eq("id", id).limit(1).single(), id });
